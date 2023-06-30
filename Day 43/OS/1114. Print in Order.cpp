458628fe8b553d6a206cac0a79eb85bdf21cde8a
Ajay Rawat <60476050+theajayrawat@users.noreply.github.com>
2023-06-30 07:01:34 +0530

    Add files via upload

---
+++ Day 43/OS/1114. Print in Order.cpp
@@ -0,0 +1,50 @@
+// Link: https://leetcode.com/problems/print-in-order/
+
+class Foo
+{
+public:
+    mutex m;
+    condition_variable cv;
+    int turn;
+    Foo()
+    {
+        turn = 0;
+    }
+
+    void first(function<void()> printFirst)
+    {
+
+        // printFirst() outputs "first". Do not change or remove this line.
+
+        printFirst();
+        turn = 1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond)
+    {
+
+        // printSecond() outputs "second". Do not change or remove this line.
+        unique_lock<mutex> lock(m);
+        while (turn != 1)
+        {
+            cv.wait(lock);
+        }
+        printSecond();
+        turn = 2;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird)
+    {
+
+        // printThird() outputs "third". Do not change or remove this line.
+        unique_lock<mutex> lock(m);
+        while (turn != 2)
+        {
+            cv.wait(lock);
+        }
+        printThird();
+        cv.notify_all();
+    }
+};
