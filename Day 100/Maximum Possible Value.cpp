dbdc9a5ced2384674ddea8d26edc433212266f6d
Ajay Rawat <60476050+theajayrawat@users.noreply.github.com>
2023-08-26 22:42:38 +0530

    Add files via upload

--- Day 100/Maximum Possible Value.cpp
+++ Day 100/Maximum Possible Value.cpp
@@ -1,3 +1,23 @@
 // Link:https://practice.geeksforgeeks.org/problems/2d3fc3651507fc0c6bd1fa43861e0d1c43d4b8a1/1
-// Time Complexity: O(N*LogN)
-// Space Complexity: O(N)
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+class Solution {
+  public:
+    long long maxPossibleValue(int N,vector<int> A, vector<int> B) {
+        // code here
+        long long int count=0;
+        long long int ans=0;
+        int mini=INT_MAX;
+        
+        for(int i=0;i<N;i++){
+            count+=(long long)(B[i]/2);
+            ans+=(long long )A[i]*(B[i]-B[i]%2);
+            if(B[i]>=2)
+            mini=min(A[i],mini);
+        }
+      
+      if(count%2!=0)ans-=mini*2;
+      return ans;
+    }
+};
--- Day 100/Shortest XY distance in Grid.cpp
+++ Day 100/Shortest XY distance in Grid.cpp
@@ -1,3 +1,59 @@
 // Link: https://practice.geeksforgeeks.org/problems/7366ce450d84b55391fc787a681c4d60de359e72/1
-// Time Complexity: O(N*LogN)
-// Space Complexity: O(N)
+// Time Complexity: O(N*M)
+// Space Complexity: O(N*M)
+
+class Solution
+{
+public:
+    int shortestXYDist(vector<vector<char>> grid, int n, int m)
+    {
+        // code here
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 'X')
+                {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        int ans = INT_MAX;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while (!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+
+            int r = it.first.first;
+            int c = it.first.second;
+            int dist = it.second;
+            if (grid[r][c] == 'Y')
+            {
+                return dist;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if (nr >= 0 and nr < n and nc >= 0 and nc < m and vis[nr][nc] == 0)
+                {
+                    q.push({{nr, nc}, dist + 1});
+                    vis[nr][nc] = 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
